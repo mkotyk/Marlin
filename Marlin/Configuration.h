857ff375d2a927da60b90a9a9cdc5d2d73a5ad26
Mark Kotyk <mkotyk@softwaresecured.com>
2020-11-04 19:11:15 -0500

    - Make Z 0 top of z axis - Make Y 0 front to y axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1137,11 +1137,11 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
-#define Z_MIN_POS 0
+#define Y_MIN_POS -Y_BED_SIZE
+#define Z_MIN_POS -57
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 57
+#define Y_MAX_POS 0
+#define Z_MAX_POS 0
 
 /**
  * Software Endstops
